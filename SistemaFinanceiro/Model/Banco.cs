c0f9989e1db1e045585680b3162ae0637f6ceb2f
ArthurBr23 <arthursilvah736@gmail.com>
2024-06-02 20:43:56 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaFinanceiro/Model/Banco.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaFinanceiro.Model
+{
+    public class Banco
+    {
+        private int _numero;
+        private string _nome;
+
+        public Banco(int numero)
+        {
+            _numero = numero;
+        }
+
+        public int Numero { get => _numero; }
+    }
+}
+
